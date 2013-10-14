0ba0b2eeff99ae7bda340eea391378f087e23da7
Amy Ton <amyhton@gmail.com>
2013-10-14 15:01:28 -0500

    Add flash message and run migration to remove password and emial attributes from current User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131011204136) do
+ActiveRecord::Schema.define(version: 20131014200008) do
 
   create_table "phases", force: true do |t|
     t.string   "name"
@@ -44,11 +44,9 @@ ActiveRecord::Schema.define(version: 20131011204136) do
   add_index "projects", ["phase_id"], name: "index_projects_on_phase_id"
 
   create_table "users", force: true do |t|
-    t.string   "password"
     t.text     "profile_pic"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email"
     t.string   "name"
   end
 
